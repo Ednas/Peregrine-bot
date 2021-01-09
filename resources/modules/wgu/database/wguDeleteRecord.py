423931744fadc1dc944219be54882fbf08c4f32a
U03A9 <justin@gemstatecyber.com>
2021-01-09 15:48:41 -0700

    Update all files. Integrate new database. Setup gitignore and .env files

---
+++ resources/modules/wgu/database/wguDeleteRecord.py
@@ -0,0 +1,14 @@
+async def wgu_delete_record(field):
+    "Deletes the selected table from the table"
+    # Check for symbols to qualify index type: username -> email
+    conx = connect()
+    cursor = conx.cursor()
+    sql = ""
+    val = (field, )
+    if field[-5] == '#':
+        sql = "DELETE FROM auth WHERE username = %s"
+    else:
+        sql = "DELETE FROM auth WHERE email = %s"
+    cursor.execute(sql, val)
+    conx.commit()
+
---
+++ resources/modules/wgu/usermanagement/wguNewUserNick.py
@@ -0,0 +1,18 @@
+
+
+import discord
+
+
+async def wgu_set_user_nick_on_join(member, channel):
+
+    # Set new members to default unverified role
+
+    try:
+
+        await member.edit(nick=NICKNAME_SCHEMA)
+
+    except Exception as e:
+        print(e)
+        errorMessage = "Failed to set nickname on new user: {}\n".format(member)
+        await channel.send(content=errorMessage)
+    return

f32c393d44cc39a3898fe8ba3535181288323a73
U03A9 <justin@gemstatecyber.com>
2021-01-15 10:51:37 -0700

    Fix a few function calls

--- resources/modules/wgu/usermanagement/wgu_add_verified_role.py
+++ resources/modules/wgu/usermanagement/wgu_add_verified_role.py
@@ -3,15 +3,15 @@ import discord
 
 async def wgu_add_verified_role(self, VERIFIED_ROLE, UNVERIFIED_ROLE, member):
        
-        try:
+    try:
 
-            await member.add_roles(
-                discord.utils.get(payload.member.guild.roles, name=VERIFIED_ROLE)
-            )
+        await member.add_roles(
+            discord.utils.get(payload.member.guild.roles, name=VERIFIED_ROLE)
+        )
 
-            await member.remove_roles(
-               discord.utils.get(payload.member.guild.roles, name=UNVERIFIED_ROLE)
-            )
+        await member.remove_roles(
+            discord.utils.get(payload.member.guild.roles, name=UNVERIFIED_ROLE)
+        )
 
     except Exception as e:
         print(e)

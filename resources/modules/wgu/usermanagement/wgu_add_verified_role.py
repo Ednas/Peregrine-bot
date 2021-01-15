077e4c4f61bd429d192e2b34718e3ea7df097964
U03A9 <justin@gemstatecyber.com>
2021-01-15 10:54:25 -0700

    Fix a few function calls

--- resources/modules/wgu/usermanagement/wgu_add_verified_role.py
+++ resources/modules/wgu/usermanagement/wgu_add_verified_role.py
@@ -6,18 +6,16 @@ async def wgu_add_verified_role(self, VERIFIED_ROLE, UNVERIFIED_ROLE, member):
     try:
 
         await member.add_roles(
-            discord.utils.get(payload.member.guild.roles, name=VERIFIED_ROLE)
+            discord.utils.get(member.guild.roles, name=VERIFIED_ROLE)
         )
 
         await member.remove_roles(
-            discord.utils.get(payload.member.guild.roles, name=UNVERIFIED_ROLE)
+            discord.utils.get(member.guild.roles, name=UNVERIFIED_ROLE)
         )
 
     except Exception as e:
         print(e)
-        errorMessage = "Failed to process verification role for new member: {}\nPlease hand verify this member or contact a bot developer".format(
-            payload.member
-        )
+        errorMessage = "Failed to process verification role for new member: {}\nPlease hand verify this member or contact a bot developer".format(member)
         await channel.send(content=errorMessage)
 
     return

88b60de3500d905ee17d3351612811240e4cf71c
U03A9 <justin@gemstatecyber.com>
2021-01-22 16:46:17 -0700

    Fix a few function calls

--- resources/modules/wgu/usermanagement/roles/wgu_enrollment_status_self_role.py
+++ resources/modules/wgu/usermanagement/roles/wgu_enrollment_status_self_role.py
@@ -4,12 +4,12 @@ async def wgu_enrollment_status_self_role(self, payload, STUDENT_EMOJI, ALUMNI_E
 
     # Assign Student role
 
-    if str(payload.emoji.name) == str(CCDC_SUB_EMOJI):
+    if str(payload.emoji.name) == str(STUDENT_EMOJI):
         await payload.member.add_roles(discord.utils.get(payload.guild.roles, name='Student'))
 
     # Assign Alumni role
 
-    if str(payload.emoji.name) == str(CCDC_SUB_EMOJI):
+    if str(payload.emoji.name) == str(ALUMNI_EMOJI):
         await payload.member.add_roles(discord.utils.get(payload.guild.roles, name='Alumni'))
     
     return

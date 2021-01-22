ac17cc088c854b38a464493ae971e14865316106
U03A9 <justin@gemstatecyber.com>
2021-01-21 20:56:46 -0700

    Fix a few function calls

--- peregrineBot.py
+++ peregrineBot.py
@@ -214,7 +214,7 @@ class peregrine(discord.Client):
                 
                 # Add initial reaction
 
-                for emoji in ('📄'):
+                for emoji in (':electric_plug:'):
                     await sub_embed_message.add_reaction(emoji)
             
             except Exception as e:

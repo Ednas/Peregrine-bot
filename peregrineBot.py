f77a3b054ec60ac46e9826790e14c88678682148
U03A9 <justin@gemstatecyber.com>
2021-01-21 22:28:38 -0700

    Fix a few function calls

--- peregrineBot.py
+++ peregrineBot.py
@@ -349,7 +349,7 @@ class peregrine(discord.Client):
             print("Sanity check. Submitted message is: {}\n from: {}".format(message.content, message.author.id))
             print("    ┕ Email is: {}".format(message.content.split(' ')[-1]))
             print("    ┕ WGU user is: {}".format(wgu_user[0]))
-            print("    ┕ Discord Username: {}".format(discord_user[0])
+            print("    ┕ Discord Username: {}".format(discord_user[0]))
 
             # Get necessary role information
             

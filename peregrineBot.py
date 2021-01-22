50bbefabba209c633ab468560a84c503f11f6083
U03A9 <justin@gemstatecyber.com>
2021-01-21 22:51:13 -0700

    Fix a few function calls

--- peregrineBot.py
+++ peregrineBot.py
@@ -23,6 +23,7 @@ from resources.modules.wgu.embeds.wgu_roles_embed import *
 from resources.modules.wgu.embeds.wgu_verification_embed import *
 from resources.modules.wgu.embeds.wgu_htb_embed import *
 from resources.modules.wgu.embeds.wgu_announcement_sub_embed import *
+from resources.modules.wgu.embeds.wgu_verify_log_embed import *
 
 # Import database modules
 

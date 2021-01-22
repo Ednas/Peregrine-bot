8d2a47fb9f4f59997462afdde5d71ade57d30ed1
U03A9 <justin@gemstatecyber.com>
2021-01-22 16:52:10 -0700

    Fix a few function calls

--- resources/modules/wgu/usermanagement/roles/wgu_subscription_self_role.py
+++ resources/modules/wgu/usermanagement/roles/wgu_subscription_self_role.py
@@ -11,36 +11,36 @@ async def wgu_subscription_self_role(self, payload, CCDC_SUB_EMOJI, NICE_SUB_EMO
 
     # Assign NICE sub role
 
-    if str(payload.emoji.name) == str(CCDC_SUB_EMOJI):
+    if str(payload.emoji.name) == str(NICE_SUB_EMOJI):
         await payload.member.add_roles(discord.utils.get(guild.roles, name='NICE Alerts'))
 
     # Assign CTF sub role
 
-    if str(payload.emoji.name) == str(CCDC_SUB_EMOJI):
+    if str(payload.emoji.name) == str(CTF_SUB_EMOJI):
         await payload.member.add_roles(discord.utils.get(guild.roles, name='CTF Alerts'))
     
     
     # Assign Foreign sub role
 
-    if str(payload.emoji.name) == str(CCDC_SUB_EMOJI):
+    if str(payload.emoji.name) == str(FOREIGN_SUB_EMOJI):
         await payload.member.add_roles(discord.utils.get(guild.roles, name='Foreign Alerts'))
     
     
     # Assign HTB sub role
 
-    if str(payload.emoji.name) == str(CCDC_SUB_EMOJI):
+    if str(payload.emoji.name) == str(HTB_SUB_EMOJI):
         await payload.member.add_roles(discord.utils.get(guild.roles, name='HTB Alerts'))
     
     
     # Assign THM sub role
 
-    if str(payload.emoji.name) == str(CCDC_SUB_EMOJI):
+    if str(payload.emoji.name) == str(THM_SUB_EMOJI):
         await payload.member.add_roles(discord.utils.get(guild.roles, name='THM Alerts'))
     
     
     # Assign OTW sub role
 
-    if str(payload.emoji.name) == str(CCDC_SUB_EMOJI):
+    if str(payload.emoji.name) == str(OTW_SUB_EMOJI):
         await payload.member.add_roles(discord.utils.get(guild.roles, name='OTW Alerts'))
     
     return

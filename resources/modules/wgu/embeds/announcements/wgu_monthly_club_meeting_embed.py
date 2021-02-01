ae42738ed42cb0b35965f66890fef40b3c8c75f9
U03A9 <justin@gemstatecyber.com>
2021-02-01 11:13:48 -0700

    Fix a few function calls

---
+++ resources/modules/wgu/embeds/announcements/wgu_monthly_club_meeting_embed.py
@@ -0,0 +1,31 @@
+import discord
+
+# Import additional modules
+
+from discord.ext import commands
+
+
+async def wgu_user_verified_success_embed(wgu_user):
+
+    # Set initial message here
+
+    successMessage = discord.Embed(
+        title="Weekly Cyber Club Meeting!",
+        description="Hello Night Owls,\n\nRemember, our monthly club meeting that occus on {DATE}.\n\n".format(wgu_user[0]),
+        colour=discord.Colour.dark_blue(),
+    )
+  
+    # Standard footer and author
+
+    successMessage.set_footer(
+        text="This is a club member run Discord officially sponsored by Western Governors University. All interactions on this server are logged. All users agree to the Discord Terms of Service"
+    )
+    successMessage.set_thumbnail(
+        url="https://cdn.discordapp.com/avatars/758331935054889020/39da98d02a59e22606e230e7afe6841a.png"
+    )
+    successMessage.set_author(
+        name="Ursa | nchri49",
+        icon_url="https://cdn.discordapp.com/avatars/454132802535817219/31626daa6b0d4a01d0213a6390c5fe3e.png",
+    )
+
+    return successMessage
---
+++ resources/modules/wgu/embeds/logs/wgu_verify_success_log_embed.py
@@ -0,0 +1,31 @@
+import discord
+
+# Import additional modules
+
+from discord.ext import commands
+import discord
+
+async def wgu_verify_log_embed(user_email, wgu_user, discord_user, new_nickname, message):
+
+    # Set initial message here
+
+    logMessage = discord.Embed(
+        title="Verification complete",
+        description="The following account has been verified\n\n   ┕ User email: {}\n  ┕ WGU Username: {}\n  ┕ Discord Username: {}\n".format(user_email, wgu_user[0], discord_user[0]),
+        colour=discord.Colour.dark_blue(),
+    )
+    
+    # Standard footer and author
+
+    logMessage.set_footer(
+        text="This is a club member run Discord officially sponsored by Western Governors University. All interactions on this server are logged. All users agree to the Discord Terms of Service"
+    )
+    logMessage.set_thumbnail(
+        url="https://cdn.discordapp.com/avatars/758331935054889020/39da98d02a59e22606e230e7afe6841a.png"
+    )
+    logMessage.set_author(
+        name="Peregrine",
+        icon_url="https://cdn.discordapp.com/avatars/716442510423228496/f293e738c3559906120db7e4d43da13e.png?size=128",
+    )
+
+    return logMessage

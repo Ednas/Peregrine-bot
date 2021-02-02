b2594f6fa9a23d6781362417f1a42bf68302d368
U03A9 <justin@gemstatecyber.com>
2021-02-01 21:37:49 -0700

    Fix log for successful verification

--- resources/modules/wgu/embeds/logs/wgu_verify_log_embed.py
+++ resources/modules/wgu/embeds/logs/wgu_verify_log_embed.py
@@ -5,7 +5,7 @@ import discord
 from discord.ext import commands
 import discord
 
-async def wgu_verify_log_embed(user_email, wgu_user, discord_user, new_nickname, message):
+async def wgu_verify_log_embed(user_email, wgu_user, discord_user, new_nickname, code, message):
 
     # Set initial message here
 

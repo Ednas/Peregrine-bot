a27c6d5e9f2c2fb6404c98bf41aeff752b579f0b
U03A9 <justin@gemstatecyber.com>
2021-02-05 19:49:51 -0700

    Add ncl embed

--- resources/modules/wgu/embeds/introductions/wgu_sos_intro_embed.py
+++ resources/modules/wgu/embeds/introductions/wgu_sos_intro_embed.py
@@ -10,9 +10,15 @@ async def wgu_sos_intro_embed():
     # Set initial message here
 
     sosMessage = discord.Embed(
-        title="Tell us a little bit about yourself",
-        description="Simeon is a Systems Administrator by day and Cyber Security and Privacy Enthusiast by night. He is a tinkerer, a hobbyist, and all around geek and nerd. Simeon got his first start working with a laptop that couldn't even run Windows XP stably at the age of 12. Troubleshooting and fixing issues as they arised, Simeon couldn't get enough. Years later, Simeon now has an in home data-center and lab that rivals most small businesses compute and storage power.\n━━━━━",
+        title="",
+        description="Discord Community Moderator | Cyber Security Professional and Automation Enthusiast | BSCSIA\n━━━━━",
         colour=discord.Colour.dark_blue(),
+    )
+
+    sosMessage.add_field(
+        name="Tell us a little bit about yourself",
+        value="Simeon is a Systems Administrator by day and Cyber Security and Privacy Enthusiast by night. He is a tinkerer, a hobbyist, and all around geek and nerd. Simeon got his first start working with a laptop that couldn't even run Windows XP stably at the age of 12. Troubleshooting and fixing issues as they arised, Simeon couldn't get enough. Years later, Simeon now has an in home data-center and lab that rivals most small businesses compute and storage power.\n━━━━━",
+        inline=False,
     )
     sosMessage.add_field(
         name="Where can we find additional information?",
@@ -28,7 +34,7 @@ async def wgu_sos_intro_embed():
         url="https://cdn.discordapp.com/attachments/756294909564289084/807437710779744266/4913771.png"
     )
     sosMessage.set_author(
-        name="SimeonOnSecurity.ch | smil289 [*]\nDiscord Community Moderator | Cyber Security Professional and Automation Enthusiast | BSCSIA",
+        name="SimeonOnSecurity.ch | smil289 [*]",
         icon_url="https://cdn.discordapp.com/attachments/756294909564289084/807437710779744266/4913771.png",
     )
 

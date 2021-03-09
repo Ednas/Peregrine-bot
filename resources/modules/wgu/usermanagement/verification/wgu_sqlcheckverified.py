772e0a54e041a7dcdf55854d42480b7d318eacf5
Justin Garcia <u03a9@Justins-MacBook-Pro.local>
2021-03-08 19:08:02 -0700

    Add module to check SQL database for existing email

--- resources/modules/wgu/usermanagement/verification/wgu_sqlcheckverified.py
+++ resources/modules/wgu/usermanagement/verification/wgu_sqlcheckverified.py
@@ -1,10 +1,10 @@
 async def wgu_sqlcheckverified(self, email, conx):
     
-    print("Sanity check:\n {}".format(email))
     cursor = conx.cursor()
     sql = "SELECT * FROM verified WHERE email LIKE %s"
     val = (email, )
     cursor.execute(sql, val)
     result = cursor.fetchall()
+    print(result)
     
     return result

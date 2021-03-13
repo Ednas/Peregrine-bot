d8518ae8d4384a7bd8eba2d95aa9cab14f0200fd
Justin Garcia <u03a9@Justins-MacBook-Pro.local>
2021-03-13 07:47:58 -0700

    Add logical check to ensure bot doesn't fail if a user submits a pin without first submitting their email

--- modules/wgu/database/database_check_ver_pin.py
+++ modules/wgu/database/database_check_ver_pin.py
@@ -14,7 +14,11 @@ async def database_check_ver_pin(database_connection, discord_id, submitted_auth
                 print("Submitted pincode is correct")
                 return bool(query_results[0][0]), bool(query_results[0][1]), str(query_results[0][2])
 
-    else:
+        else:
 
-        print("Submitted is incorrect")
-        return False
+            print("Submitted is incorrect")
+            return False
+
+    if bool(len(query_results) == 0) is True:
+        print("This user does not have a pin in the database")
+        return

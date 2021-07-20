70cef52a6601571e1a0ca62883dadb6f0eb38204
Justin Garcia <u03a9@Justins-MacBook-Pro.local>
2021-07-19 19:53:08 -0600

    modify to fit DiscordUser

--- modules/wgu/database/database_audit_members.py
+++ modules/wgu/database/database_audit_members.py
@@ -4,7 +4,7 @@ async def database_audit_members(database_connection, email):
     # Execute SQL query
 
     cursor = database_connection.cursor(prepared=True)
-    sql_query = "SELECT Email, DiscordID, VerifiedDate, DiscordNickname FROM verified WHERE Email = %s"
+    sql_query = "SELECT Email, DiscordID, DiscordUser FROM verified WHERE Email = %s"
     parameterized_values = (email, )
     cursor.execute(sql_query, parameterized_values)
     query_results = cursor.fetchall()
@@ -16,7 +16,7 @@ async def database_audit_members(database_connection, email):
             print(f"{email} exists in the verified database")
             print(f"Variables are:\n[0]{query_results[0][0]}\n[1]{query_results[0][1]}\n[2]{query_results[0][2]}\n[3]{query_results[0][3]}")
 
-        return bool(True), query_results[0][0], query_results[0][1], query_results[0][3]
+        return bool(True), query_results[0][0], query_results[0][1], query_results[0][3]2
 
     else:
 
